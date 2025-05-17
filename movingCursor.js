b4da240e0be2bbd9426ae540d4a3c7d15372d761
CALEB OUEDRAOGO <calebouedraogo@CALEBs-MacBook-Pro.local>
2025-05-17 04:19:31 -0400

    Created a method to update the position of each node

--- movingCursor.js
+++ movingCursor.js
@@ -7,4 +7,14 @@ class LimbSystem {
             this.nodes.push({ x: 0, y: 0});
         }
     }
+
+    update(x, y) {
+        this.nodes[0] = { x, y};
+        for (let i = 1; i < this.nodes.length; i++) {
+            const prev = this.nodes[i - 1];
+            const node = this.nodes[i];
+            node.x += (prev.x - node.x) * this.speed;
+            node.y += (prev.y - node.y) * this.speed;
+        }
+    }
 }
